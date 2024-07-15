5d76d615841bf39dd19d422287a0c6fababadb92
marcosg91 <marcos_g91@hotmail.com>
2024-07-15 16:29:47 -0300

    01-info/programacion-imperativa - organizacion del codigo en java con programacion imperativa

---
+++ imperativa/organizacion/OrganizacionCodigo.java
@@ -0,0 +1,31 @@
+package imperativa.organizacion;
+
+public class OrganizacionCodigo {
+    public static void main(String[]args){
+        System.out.println("Hola mundo!");
+
+        //lineas de codigo = sentencias
+
+        //declaraciones - aqui defino el tipo de dato
+
+        int nombreVariable;
+
+        //asignacion o inicializacion
+        nombreVariable = 1;
+
+        //asignacion
+        nombreVariable = 2;
+        nombreVariable = 3;
+        nombreVariable = 4; //la maquina toma el ultimo valor
+
+        //declaracion e inicializacion
+
+        int nombreVariable2 = 15;
+
+        //declaro e inicializo varias varibales
+        int valor1, valor2 = 2;
+
+        System.out.println("Valor1"+":"+nombreVariable);
+    }
+
+}
