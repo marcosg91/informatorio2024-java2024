1a2572e24ea444720f1d40a9220767c898802db6
marcosg91 <marcos_g91@hotmail.com>
2024-09-24 21:27:14 -0300

    Actualización de .gitignore y BookController

--- spring/info2024-primera-app/info2024-primera-app/src/main/java/com/practica/info2024_primera_app/controller/BookController.java
+++ spring/info2024-primera-app/info2024-primera-app/src/main/java/com/practica/info2024_primera_app/controller/BookController.java
@@ -1,14 +1,22 @@
-package controller;
+package com.practica.info2024_primera_app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController //anotacion a nivel de clase
 public class BookController {
     //Get --> Obtener
-    @GetMapping("/aplicacion/v1/despedida")
+    //http://localhost:8080/aplicacion/v1/despedida
+    @GetMapping("/aplicacion/v1/despedida") //anotacion a nivel de metodo
     public String goodByeWorld(){
         return "Adios mundo cruel";
+    }
+    //http://localhost:8080/aplicacion/v1/saludo?nombre=Marcos
+    @GetMapping("/aplicacion/v1/saludo")
+    public String helloWorld(@RequestParam(required = true, name="nombre") String nombre){
+        //(@RequestParam(required = true, name="nombre") String nombre) anotacion a nivel de atributo
+        return "Hola " + nombre;
     }
 
 }
